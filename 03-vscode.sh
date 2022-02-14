7b45ed6629edf7f5a7de156fde09ac11f0570666
Babu Natarajan <babunatarajan6@gmail.com>
2022-02-14 13:19:58 -0500

    Update 03-vscode.sh

--- 03-vscode.sh
+++ 03-vscode.sh
@@ -4,9 +4,12 @@ wget https://github.com/coder/code-server/releases/download/v3.12.0/code-server_
 apt install /tmp/code-server_3.12.0_amd64.deb 
 systemctl enable --now code-server@$USER
 
-sed -i 's/127.0.0.1/0.0.0.0/g' $HOME/.config/code-server/config.yaml
-sed -i 's/8080/9091/g' $HOME/.config/code-server/config.yaml
+sed -i "s/127.0.0.1/0.0.0.0/g" $HOME/.config/code-server/config.yaml
+
+sed -i "s/8080/9091/g" $HOME/.config/code-server/config.yaml
+
 systemctl restart code-server@$USER.service
 systemctl status code-server@$USER.service
 cat $HOME/.config/code-server/config.yaml
+echo ''
 echo 'Open the FQDN and login using, password in ~/.config/code-server/config.yaml '
